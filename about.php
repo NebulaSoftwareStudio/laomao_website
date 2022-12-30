80572803e8bdc0d40b5e296d8e2dc3990cc4b44a
moonlightwatch <moonlightwatch@hotmail.com>
2022-12-30 11:26:29 +0800

    fix: about 页面参数检查不严导致的任意文件执行问题

--- about.php
+++ about.php
@@ -1,11 +1,13 @@
 <?php
 
-switch($_GET["classify"]){
-    case 'privacy': $page_title='隐私权政策';break;
+$classify = $_GET["classify"]; // 使用变量保存输入，以便使用默认值
+
+switch($classify){
+    // case 'privacy': $page_title='隐私权政策';break; // 移除未使用的页面链接
     case 'EULA': $page_title='服务条款';break;
     case 'about': $page_title='关于本站';break;
     case 'license': $page_title='开放源代码许可';break;
-    default: $page_title='';
+    default: $page_title='';$classify="about"; // 对意料之外的输入，使用默认值
 }
 
 ?>
@@ -17,7 +19,7 @@ switch($_GET["classify"]){
 <body>
 <?php require "common/theme/eyefind/layout/menu.php"; ?>
 
-<?php require "common/theme/eyefind/content/about/" . $_GET["classify"] . ".php"; ?>
+<?php require "common/theme/eyefind/content/about/" . $classify . ".php"; ?>
 
 <?php require "common/theme/eyefind/layout/footer.php"; ?>
 </body>
